13e6b45b93529f54c50bd1712026eb12f8b10f70
seoyun <dhn04100@gmail.com>
2022-07-07 17:35:06 +0900

    wip

--- students/seoyun/westagram/users/models.py
+++ students/seoyun/westagram/users/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class User(models.Model):
-    name         = models.CharField(max_length=20),
+    name         = models.CharField(max_length=25),
     email        = models.EmailField(max_length=250, unique=True),
     password     = models.CharField(max_length=300),
     phone_number = models.IntegerField(max_length=50)
