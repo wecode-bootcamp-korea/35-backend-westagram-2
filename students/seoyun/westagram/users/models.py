38f87e25339b2ff2c503b93f5ff84665c99cfacb
seoyun <dhn04100@gmail.com>
2022-07-07 20:35:19 +0900

    Add: models.py user class with code align

--- students/seoyun/westagram/users/models.py
+++ students/seoyun/westagram/users/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
 class User(models.Model):
-    name = models.CharField(max_length=50)
-    email = models.CharField(max_length=50, unique=True)
-    password = models.CharField(max_length=250)
+    name         = models.CharField(max_length=50)
+    email        = models.CharField(max_length=50, unique=True)
+    password     = models.CharField(max_length=250)
     phone_number = models.CharField(max_length=50)
 
     class Meta:
