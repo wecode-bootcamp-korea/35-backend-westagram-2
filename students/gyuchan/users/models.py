673171a585bd679732649635468fb6e5c592e8d0
gyuchan <schk9611@gmail.com>
2022-07-07 15:41:40 +0900

    Modify: models.py의 속성들의 필드 수정

--- students/gyuchan/users/models.py
+++ students/gyuchan/users/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 class User(models.Model):
-    name = models.CharField(max_length=100)
-    email = models.EmailField(max_length=100)
-    password = models.CharField(max_length=100)
-    phone_number = models.IntegerField()
+    name         = models.CharField(max_length=100)
+    email        = models.CharField(max_length=100)
+    password     = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=100)
 
     class Meta:
         db_table = "users"
