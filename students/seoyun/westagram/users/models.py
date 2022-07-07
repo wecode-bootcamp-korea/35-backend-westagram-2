e02b8dd8ba1e6136bf3c0dd9a69be44a032344b7
seoyun <dhn04100@gmail.com>
2022-07-07 20:31:28 +0900

    Add: models.py user class

---
+++ students/seoyun/westagram/users/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class User(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.CharField(max_length=50, unique=True)
+    password = models.CharField(max_length=250)
+    phone_number = models.CharField(max_length=50)
+
+    class Meta:
+        db_table = 'users'
