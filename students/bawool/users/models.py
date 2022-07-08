780e3ad83e3fd2feeeeeb6171df1eb2362651f5e
성바울 <bawool@seongbaul-ui-MacBookPro.local>
2022-07-08 14:19:20 +0900

    westagram 모델링(줄바꿈수정)

--- students/bawool/users/models.py
+++ students/bawool/users/models.py
@@ -8,7 +8,6 @@ class User(models.Model):
     email      = models.CharField(max_length=200, unique=True)
     password   = models.CharField(max_length=40)
     phone      = models.CharField(max_length=40,  unique=True)
-
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
--- students/bawool/users/views.py
+++ students/bawool/users/views.py
@@ -1,9 +1,45 @@
 import json
 
+from time import clock_settime
+
 from django.http import JsonResponse
 from django.views import View
 
 from .models import User
 
 # Create your views here.
+
+class SignupView(View):
+    def post(self, request):
+        try:
+            data     = json.loads(request.body)
+            name     = data['name']
+            email    = data['email']
+            password = data['password']
+            phone    = data['phone']
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            User.objects.create(
+                name     = name,
+                email    = email,
+                password = password,
+                phone    = phone
+                )
+        
 
