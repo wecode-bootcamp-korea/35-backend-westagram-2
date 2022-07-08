c98225cca1f06e37327a4594f2010739704692e3
성바울 <bawool@seongbaul-ui-MacBookPro.local>
2022-07-08 14:44:18 +0900

    westagram 모델링(패스워드수정)

--- students/bawool/users/models.py
+++ students/bawool/users/models.py
@@ -6,7 +6,7 @@ from django.db import models
 class User(models.Model): 
     name       = models.CharField(max_length=40,  unique=True)
     email      = models.CharField(max_length=200, unique=True)
-    password   = models.CharField(max_length=40)
+    password   = models.CharField(max_length=200)
     phone      = models.CharField(max_length=40,  unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
