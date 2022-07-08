95de5e2469745dfdb4e2e76385503fc60cce0385
seoyun <dhn04100@gmail.com>
2022-07-08 14:20:33 +0900

    Add: models.py created_at, updated_at

--- students/seoyun/westagram/users/models.py
+++ students/seoyun/westagram/users/models.py
@@ -6,5 +6,8 @@ class User(models.Model):
     password     = models.CharField(max_length=250)
     phone_number = models.CharField(max_length=50)
 
+    created_at   = models.DateTimeField(auto_now_add=True)
+    updated_at   = models.DateTimeField(auto_now=True)    
+    
     class Meta:
         db_table = 'users'
