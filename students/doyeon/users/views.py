ae0741975d99afac72cc8697ed8d0f32c5c317bb
apple <apple@apples-MacBook-Pro.local>
2022-07-11 15:47:42 +0900

    - EDIT : users/views.py - EDIT : users/views.py check_regx()

--- students/doyeon/users/views.py
+++ students/doyeon/users/views.py
@@ -3,7 +3,6 @@ import re
 
 from django.http import JsonResponse
 from django.views import View
-from django.utils import timezone
 
 from .models import User
 
@@ -20,33 +19,26 @@ class UserView(View):
             regx_email      = '^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
             regx_password   = '^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$'
 
-            if check_regx(regx_email, email):
-                if User.objects.filter(email=email).exists():
-                    raise ValueError("EXISTED E_MAIL")
-            if check_regx(regx_password, password):
-                if User.objects.filter(phone_number=phone_number).exists():
-                    raise ValueError("EXISTED PHONE_NUMBER")
+            check_regx(regx_email, email)
+            if User.objects.filter(email = email).exists():
+                raise ValueError("EXISTED E_MAIL")
+            check_regx(regx_password, password)
+            if User.objects.filter(phone_number = phone_number).exists():
+                raise ValueError("EXISTED PHONE_NUMBER")
+
+            User.objects.create(
+                name         = name,
+                email        = email,
+                password     = password,
+                phone_number = phone_number,
+            )
+            return JsonResponse({"message": "SUCCESS"}, status=201)
 
         except KeyError:
             return JsonResponse({"message": "KEY_ERROR"}, status=400)
         except ValueError as e:
             return JsonResponse({"message": f"{e}"}, status=400)
 
-        else:
-            user = User.objects.create(
-                name         = name,
-                email        = email,
-                password     = password,
-                phone_number = phone_number,
-                create_at    = timezone.now(),
-                update_at    = timezone.now(),
-            )
-            return JsonResponse({"message": "SUCCESS"}, status=201)
-
 def check_regx(pattern, data):
-    if re.compile(pattern).match(data) is None:
+    if not re.compile(pattern).match(data):
         raise ValueError("INVALID_PASSWORD")
-        return False
-    else:
-        return True
-
