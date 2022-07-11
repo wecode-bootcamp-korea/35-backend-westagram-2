b1c7a32cc50210bac8ebf72babc9048ea65cade3
apple <apple@apples-MacBook-Pro.local>
2022-07-11 09:27:56 +0900

    - EDIT : users/views.py

--- students/doyeon/users/views.py
+++ students/doyeon/users/views.py
@@ -10,15 +10,15 @@ from .models import User
 class UserView(View):
     def post(self, request):
         try:
-            data = json.loads(request.body)
+            data            = json.loads(request.body)
 
-            name = data["name"]
-            email = data["email"]
-            password = data["password"]
-            phone_number = data["phone_number"]
+            name            = data["name"]
+            email           = data["email"]
+            password        = data["password"]
+            phone_number    = data["phone_number"]
 
-            regx_email = '^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
-            regx_password = '^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$'
+            regx_email      = '^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
+            regx_password   = '^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$'
 
             if check_regx(regx_email, email):
                 if User.objects.filter(email=email).exists():
@@ -34,15 +34,14 @@ class UserView(View):
 
         else:
             user = User.objects.create(
-                name=name,
-                email=email,
-                password=password,
-                phone_number=phone_number,
-                create_at=timezone.now(),
-                update_at=timezone.now(),
+                name         = name,
+                email        = email,
+                password     = password,
+                phone_number = phone_number,
+                create_at    = timezone.now(),
+                update_at    = timezone.now(),
             )
             return JsonResponse({"message": "SUCCESS"}, status=201)
-
 
 def check_regx(pattern, data):
     if re.compile(pattern).match(data) is None:
